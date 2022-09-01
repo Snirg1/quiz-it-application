375e4a35bf41df842f2a30c2682bb05d6fc9096d
Snirg1 <snirgu1@gmail.com>
2022-09-01 18:03:14 +0300

    Create Quiz + DB working

--- backend/src/db-config.js
+++ backend/src/db-config.js
@@ -1,6 +1,5 @@
 module.exports = {
   database:
   "mongodb+srv://Snir:Snir1964@cluster0.pzz1ehe.mongodb.net/?retryWrites=true&w=majority"
-    // "mongodb+srv://hritikkumar:hritikkumar@ristorante-con-fusion.j3jnj.mongodb.net/quiz?retryWrites=true&w=majority",
 };
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,6 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  // apiKey: "AIzaSyC04aheV_N4L4rno3HLaRBxt3qdQHNIVKk",
-  // authDomain: "quizz-app-ngu.firebaseapp.com",
-  // projectId: "quizz-app-ngu",
-  // storageBucket: "quizz-app-ngu.appspot.com",
-  // messagingSenderId: "786706143626",
-  // appId: "1:786706143626:web:93f1e84605afb83d141073",
-  // measurementId: "G-H9HTHQCLVH",
-
   apiKey: "AIzaSyAp0IB-zmCINEGEYt7Hjru2ptOa_R9Rjrg",
   authDomain: "the-other-side-e1c97.firebaseapp.com",
   projectId: "the-other-side-e1c97",
