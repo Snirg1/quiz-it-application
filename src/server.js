27e20998b146c2b698ef91e4704dc7caeee069ab
Snirg1 <snirgu1@gmail.com>
2022-09-04 16:52:58 +0300

    deploy client

--- src/server.js
+++ src/server.js
@@ -13,12 +13,12 @@ app.use(express.json());
 app.use("/API/users", userRoute);
 app.use("/API/quizzes", quizzesRoute);
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 // app.use(express.static('build')));
 
-app.use("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.use("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('/client/build'))
@@ -26,9 +26,9 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
-app.use("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.use("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 // Listening to APIs
 app.listen(process.env.PORT || 8000, () =>
