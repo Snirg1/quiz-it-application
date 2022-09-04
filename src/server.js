e70bf80584bbaaa24c3d0051d6e1178d66ee0afa
Snirg1 <snirgu1@gmail.com>
2022-09-04 18:15:11 +0300

    deploy client

--- src/server.js
+++ src/server.js
@@ -4,29 +4,28 @@ const app = express();
 const path = require("path");
 const userRoute = require("./Routes/Users");
 const quizzesRoute = require("./Routes/Quizzes");
+//
+//
+// app.use(express.static("/../client/public/"));
+//
+// app.use(express.static('/client/build'))
+// app.use("*", (req, res) => {
+//     res.sendFile('/../client/build/index.html');
+// });
 
 
-app.use(express.static(path.join(__dirname, "/client/build/")));
+app.use(express.static('/../client/public'));
 
 // Middleware
 app.use(express.json());
 app.use("/API/users", userRoute);
 app.use("/API/quizzes", quizzesRoute);
 
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.use(express.static('build')));
+app.use(express.static( '/../client/build'));
 
-// app.use("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
-
-    app.use(express.static('/client/build'))
-    app.use("*", (req, res) => {
-        res.sendFile('/../client/build/index.html');
-    });
-// app.use("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
+app.use("*", (req, res) => {
+    res.sendFile("/../client/public/index.html");
+});
 
 // Listening to APIs
 app.listen(process.env.PORT || 8000, () =>
