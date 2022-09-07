baaa850551e0f6e5917c8ca8fc11738bd24fbfd0
idanmorem <idanmorem@gmail.com>
2022-09-07 20:28:34 +0300

    The "Back to dashboard" button at the about page is redirecting to the dashboard properly.

--- client/src/screens/About/About.js
+++ client/src/screens/About/About.js
@@ -16,6 +16,7 @@ const About = () => {
 
 
     // here we check if we need to be redirected to other screen (also other URL)
+    //TODO: check at deployment heroku
     if (valid === 'true') return <Redirect to={path}/>;
     return (
         <div id="about">
@@ -23,8 +24,10 @@ const About = () => {
                 <div id="logo-name">
                     <b style={{ fontweight: 600 }}>About</b>
                 </div>
+                {/*TODO: create a card for the about content*/}
                 <div className="about-content">
                     <b>
+                        {/*TODO: create about content*/}
                         TESTING About
                     </b>
                 </div>
