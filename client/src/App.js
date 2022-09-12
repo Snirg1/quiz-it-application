98c6c541f449f05e01fe4dd16392a123885aff0f
idanmorem <idanmorem@gmail.com>
2022-09-12 12:26:05 +0300

    Score is working properly

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ import About from './screens/About/About'
 
 const App = () => {
    const [user, setUser] = useState({})
-   const [mainQuizCode, setMainQuizCode] = useState('631c77001d4480a3d04991e3')
+   const [mainQuizCode, setMainQuizCode] = useState('631eef269a4e04a21cdef95c')
 
    // useEffect hook is invoked when App component is mounted
    // Also, when a user's value changes
