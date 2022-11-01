cb85bef7714eb0ad5ece3d7409dfb9301d6d8570
Snirg1 <snirgu1@gmail.com>
2022-11-01 09:27:02 +0200

    style

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
-'use strict'
-const express = require('express')
-const app = express()
-const path = require('path')
-const userRoute = require('./Routes/Users')
-const quizzesRoute = require('./Routes/Quizzes')
-const appRoot = require('../rootPath')
+"use strict";
+const express = require("express");
+const app = express();
+const path = require("path");
+const userRoute = require("./Routes/Users");
+const quizzesRoute = require("./Routes/Quizzes");
+const appRoot = require("../rootPath")
 
-app.use(express.static('/client/public'))
+app.use(express.static('/client/public'));
 
 // Middleware
-app.use(express.json())
-app.use('/API/users', userRoute)
-app.use('/API/quizzes', quizzesRoute)
+app.use(express.json());
+app.use("/API/users", userRoute);
+app.use("/API/quizzes", quizzesRoute);
 
 // serve static assets
-app.use(express.static(path.join(appRoot, '/client/build')))
-app.use('*', (req, res) => {
-   res.sendFile(path.join(appRoot, '/client/public/index.html'))
-})
+app.use(express.static(path.join(appRoot,'/client/build')));
+app.use("*", (req, res) => {
+    res.sendFile(path.join(appRoot,"/client/public/index.html"));
+});
 
 // Listening to APIs
-app.listen(process.env.PORT || 8000, () =>
-   console.log('Listening on Port 8000'),
-)
+app.listen(process.env.PORT || 6000, () =>
+    console.log("Listening on Port 5000")
+);
